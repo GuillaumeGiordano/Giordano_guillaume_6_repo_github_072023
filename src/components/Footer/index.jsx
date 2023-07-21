44ccc7dc97e12e041767b10a8d2c68312c498ccf
Wy <wy@mail.com>
2023-07-21 20:30:25 +0200

    update case

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import '../../styles/footer.css'
+import logo from '../../assets/vector.svg';
+
+export default function Footer() {
+    return (
+        <footer className='footer'>
+            <h2>K<img src={logo} className="house-logo" alt="house logo" />sa</h2>
+            <p>&copy; 2020 Kasa. All rights reserved.</p>
+        </footer>
+    )
+}
