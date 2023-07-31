f3df82c58aa18d5f03997c364d153785ed50e3ca
GuillaumeGiordano <guigoo.giordano@gmail.com>
2023-07-31 11:22:47 +0200

    E7_logement => EC
    j'ai reussi à afficher les étoiles
    reste collapse à modifier
    reste carrousselle à faire

--- src/components/Logement/index.jsx
+++ src/components/Logement/index.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Collapse from '../Collapse'
+import star_full from '../../assets/star_full.png'
+import star_empty from '../../assets/star_empty.png'
 
 export default function Logement({ data }) {
   const { id, title, cover, location, host, tags, rating, description } = data
+
+  function useStars(rating) {
+    const [stars, setStars] = useState([])
+
+    useEffect(() => {
+      for (let i = 1; i <= 5; i++) {
+        if (i <= rating) {
+          setStars((star) => [...star, 'full'])
+        } else {
+          setStars((star) => [...star, 'empty'])
+        }
+      }
+    }, [rating])
+
+    return stars
+  }
 
   console.log(data)
 
@@ -28,7 +46,27 @@ export default function Logement({ data }) {
             <p className="profil__name">{host.name}</p>
             <img className="profil__img" src={host.picture} alt="" />
           </div>
-          <div className="stars">{rating}</div>
+          <div className="stars">
+            {useStars(rating).map((star) =>
+              star === 'full' ? (
+                <img
+                  src={star_full}
+                  className=""
+                  alt="star full pink"
+                  width="24px"
+                  height="24px"
+                />
+              ) : (
+                <img
+                  src={star_empty}
+                  className=""
+                  alt="star empty grey"
+                  width="24px"
+                  height="24px"
+                />
+              )
+            )}
+          </div>
         </div>
       </div>
 
